c42c22a9343295eb611352f4ed437ba2de2c7b48
Yuriy Cherkasov <ycherkasov2015@gmail.com>
2016-09-14 12:44:20 +0300

    Transfer progect done

--- README.md
+++ README.md
@@ -6,9 +6,14 @@ CodeCare company site - Front End
 Installation
 ------------
 
+Install npm serve:
+npm install -g serve
+
 ```
+Install components:
 npm install
 bower install
+
 ```
 
 
--- public/core/controllers/home.controller.js
+++ public/core/controllers/home.controller.js
@@ -9,9 +9,19 @@ angular
     $scope.slideIndex = 0;
     
     $scope.homePageSlider = function(){
-    	$interval(function(){
-    		$scope.slideIndex = ($scope.slideIndex + 1 == 3) ? 0 : $scope.slideIndex + 1;
-    	}, 5000)
+      $interval(function(){
+        $scope.slideIndex = ($scope.slideIndex + 1 == 3) ? 0 : $scope.slideIndex + 1;
+      }, 5000)
     };
     $scope.homePageSlider();
+
+    $scope.viewExample = "apple";
+
+    $scope.changeExample = function(e){
+      var target = e.target;
+      if(target.tagName == 'IMG'){
+        $scope.viewExample = target.alt;
+      }
+      
+    }
   }
