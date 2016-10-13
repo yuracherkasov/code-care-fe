cde121c3c0eb1fa0a8a875ccd2559550263b9cdd
Yuriy Cherkasov <ycherkasov2015@gmail.com>
2016-10-13 20:20:08 +0300

    FB api

--- public/core/services/facebook.service.js
+++ public/core/services/facebook.service.js
@@ -16,14 +16,25 @@ angular
       //   return deferred.promise;
       //  }
       baseSettingFacebook: function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-          return;
-        }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
+        // var js, fjs = d.getElementsByTagName(s)[0];
+        // if (d.getElementById(id)) {
+        //   return;
+        // }
+        // js = d.createElement(s);
+        // js.id = id;
+        // js.src = "//connect.facebook.net/en_US/sdk.js";
+        // fjs.parentNode.insertBefore(js, fjs);
+
+
+        //appId: '1000008076787760',
+
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  // js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.8&appId=174816672967950";
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1000008076787760";
+  fjs.parentNode.insertBefore(js, fjs);
+
       }
     };
 
