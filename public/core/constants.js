22d66ae6f0649e9e02a6cca2ef4ee71af903903e
Victor Iskra <viktor@code-care.pro>
2017-03-16 12:22:36 +0200

    Set global API url

--- public/core/constants.js
+++ public/core/constants.js
@@ -1,8 +1,6 @@
 angular
 	.module('ccApp')
 	.constant('config', {
-		devs_API: 'FAKE_SERVER/devs.json',
-		//devProfile_API: 'FAKE_SERVER/devProfile.json',
-		projects_API: 'FAKE_SERVER/projects.json'
-	})
+		API: 'site.code-care.pro/api/'
+	});
 
