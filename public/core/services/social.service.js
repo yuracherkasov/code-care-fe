57273fe063cb1c795345f4dc3e5d59495eb50e3b
Yuriy Cherkasov <ycherkasov2015@gmail.com>
2017-04-17 14:21:29 +0300

    Fix facebook load post

--- public/core/services/social.service.js
+++ public/core/services/social.service.js
@@ -1,29 +1,41 @@
 angular
   .module("ccApp")
-  .factory('networkService', function($q) {
+  .factory('networkService', function($q, $window) {
     return {
       FbInit: function(d, s, id) {
+        if($window.FB){
+          delete $window.FB
+        };
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1000008076787760";
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
         fjs.parentNode.insertBefore(js, fjs);
       },
+
       TwInit: function(d, s, id){
         var q = $q.defer();
-        var js, fjs = d.getElementsByTagName(s)[0],
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://platform.twitter.com/widgets.js";
-        fjs.parentNode.insertBefore(js, fjs);
-        js.onload = function() {
-          if(twttr){
-            q.resolve();
-          } else {
-            q.reject()
+
+        if ($window.twttr){
+          setTimeout(function(){
+            q.resolve()
+          }, 100)
+        } else {
+          var js, fjs = d.getElementsByTagName(s)[0],
+          js = d.createElement(s);
+          js.id = id;
+          js.src = "https://platform.twitter.com/widgets.js";
+          fjs.parentNode.insertBefore(js, fjs);
+          js.addEventListener( "load", onloadApi)
+          function onloadApi() {
+            if($window.twttr){
+              q.resolve();
+            } else {
+              q.reject()
+            }
+            
           }
-          
         }
+               
         return q.promise;
       }
     };
